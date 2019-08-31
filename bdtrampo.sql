fbfee83edcfeb571949dbe3db080e52e1733fcd9
Bruno Silva <brucandido@live.com>
2019-08-31 12:09:31 -0300

    Database without anoAtuacao on tbprestador

--- bdtrampo.sql
+++ bdtrampo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 29-Ago-2019 às 22:33
--- Versão do servidor: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Host: localhost
+-- Generation Time: Aug 31, 2019 at 05:07 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,69 +25,68 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbcliente`
+-- Table structure for table `tbcliente`
 --
 
 CREATE TABLE `tbcliente` (
   `idCliente` int(11) NOT NULL,
-  `nomeCliente` varchar(40) NOT NULL,
-  `emailCliente` varchar(80) NOT NULL,
-  `senhaCliente` varchar(20) NOT NULL,
-  `contatoCliente` varchar(15) NOT NULL,
-  `dataNascCliente` date NOT NULL,
-  `sexoCliente` varchar(5) DEFAULT NULL,
-  `cnpjCliente` varchar(14) DEFAULT NULL,
-  `cpfCliente` varchar(14) NOT NULL,
-  `ufCliente` varchar(2) NOT NULL,
-  `cidadeCliente` varchar(25) NOT NULL,
-  `logradouroCliente` varchar(40) NOT NULL,
-  `bairroCliente` varchar(20) NOT NULL,
-  `numCasaCliente` int(11) NOT NULL,
-  `complementoCliente` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nomeCliente` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
+  `emailCliente` varchar(80) CHARACTER SET latin1 DEFAULT NULL,
+  `senhaCliente` varchar(200) CHARACTER SET latin1 DEFAULT NULL,
+  `contatoCliente` varchar(15) CHARACTER SET latin1 DEFAULT NULL,
+  `dataNascCliente` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  `sexoCliente` varchar(5) CHARACTER SET latin1 DEFAULT NULL,
+  `cnpjCliente` varchar(14) CHARACTER SET latin1 DEFAULT NULL,
+  `cpfCliente` varchar(14) CHARACTER SET latin1 DEFAULT NULL,
+  `ufCliente` varchar(2) CHARACTER SET latin1 DEFAULT NULL,
+  `cidadeCliente` varchar(200) CHARACTER SET latin1 DEFAULT NULL,
+  `logradouroCliente` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
+  `bairroCliente` varchar(200) CHARACTER SET latin1 DEFAULT NULL,
+  `numCasaCliente` int(11) DEFAULT NULL,
+  `complementoCliente` varchar(45) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `tbcliente`
+-- Dumping data for table `tbcliente`
 --
 
 INSERT INTO `tbcliente` (`idCliente`, `nomeCliente`, `emailCliente`, `senhaCliente`, `contatoCliente`, `dataNascCliente`, `sexoCliente`, `cnpjCliente`, `cpfCliente`, `ufCliente`, `cidadeCliente`, `logradouroCliente`, `bairroCliente`, `numCasaCliente`, `complementoCliente`) VALUES
-(8, 'Roberval', 'adm@adm.com', '12345678', '', '2019-08-29', 'M', NULL, '986.069.630-62', 'SP', '', 'Rua JosÃ© Amato', 'Cidade Tiradentes', 54, 'A');
+(25, 'Bruno', 'adm@adm.com', 'cidadeCliente', NULL, '13/01/2002', 'M', NULL, '494.022.368-05', 'SP', '', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbprestador`
+-- Table structure for table `tbprestador`
 --
 
 CREATE TABLE `tbprestador` (
   `idPrestador` int(11) NOT NULL,
-  `nomePrestador` varchar(40) NOT NULL,
-  `sexoPrestador` varchar(10) DEFAULT NULL,
-  `emailPrestador` varchar(80) NOT NULL,
-  `senhaPrestador` varchar(20) NOT NULL,
-  `contatoPrestador` varchar(15) NOT NULL,
-  `cpfPrestador` varchar(14) NOT NULL,
-  `dataNascPrestador` date NOT NULL,
-  `logradouroPrestador` varchar(20) NOT NULL,
-  `ufPrestador` varchar(2) NOT NULL,
-  `cidadePrestador` varchar(25) NOT NULL,
-  `bairroPrestador` varchar(25) NOT NULL,
-  `numCasaPrestador` int(11) NOT NULL,
-  `complementoPrestador` varchar(45) NOT NULL,
-  `cnpjPrestador` int(15) DEFAULT NULL,
-  `servicoPrestador` varchar(200) NOT NULL,
-  `cargoPrestador` varchar(20) NOT NULL,
-  `anoAtuacaoPrestador` varchar(4) NOT NULL,
-  `curriculoPrestador` blob,
-  `disponivelEmpregoPrestador` varchar(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nomePrestador` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
+  `sexoPrestador` varchar(10) CHARACTER SET latin1 DEFAULT NULL,
+  `emailPrestador` varchar(80) CHARACTER SET latin1 DEFAULT NULL,
+  `senhaPrestador` varchar(200) CHARACTER SET latin1 DEFAULT NULL,
+  `contatoPrestador` varchar(15) CHARACTER SET latin1 DEFAULT NULL,
+  `cpfPrestador` varchar(14) CHARACTER SET latin1 DEFAULT NULL,
+  `dataNascPrestador` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  `logradouroPrestador` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  `ufPrestador` varchar(2) CHARACTER SET latin1 DEFAULT NULL,
+  `cidadePrestador` varchar(25) CHARACTER SET latin1 DEFAULT NULL,
+  `bairroPrestador` varchar(25) CHARACTER SET latin1 DEFAULT NULL,
+  `numCasaPrestador` int(11) DEFAULT NULL,
+  `complementoPrestador` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
+  `cnpjPrestador` varchar(15) CHARACTER SET latin1 DEFAULT NULL,
+  `servicoPrestador` varchar(200) CHARACTER SET latin1 DEFAULT NULL,
+  `cargoPrestador` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `curriculoPrestador` blob DEFAULT NULL,
+  `disponivelEmpregoPrestador` varchar(3) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `tbprestador`
+-- Dumping data for table `tbprestador`
 --
 
-INSERT INTO `tbprestador` (`idPrestador`, `nomePrestador`, `sexoPrestador`, `emailPrestador`, `senhaPrestador`, `contatoPrestador`, `cpfPrestador`, `dataNascPrestador`, `logradouroPrestador`, `ufPrestador`, `cidadePrestador`, `bairroPrestador`, `numCasaPrestador`, `complementoPrestador`, `cnpjPrestador`, `servicoPrestador`, `cargoPrestador`, `anoAtuacaoPrestador`, `curriculoPrestador`, `disponivelEmpregoPrestador`) VALUES
-(10, 'Leonardo', 'M', 'leo@leo.com', '12345678', '', '986.069.630-62', '0000-00-00', 'Rua JosÃ© Amato', 'SP', 'sp ', 'Cidade Tiradentes', 54, 'A', NULL, 'Teatro', '', '1995', NULL, 'yes');
+INSERT INTO `tbprestador` (`idPrestador`, `nomePrestador`, `sexoPrestador`, `emailPrestador`, `senhaPrestador`, `contatoPrestador`, `cpfPrestador`, `dataNascPrestador`, `logradouroPrestador`, `ufPrestador`, `cidadePrestador`, `bairroPrestador`, `numCasaPrestador`, `complementoPrestador`, `cnpjPrestador`, `servicoPrestador`, `cargoPrestador`, `curriculoPrestador`, `disponivelEmpregoPrestador`) VALUES
+(10, 'Leonardo', 'M', 'leo@leo.com', '12345678', '', '986.069.630-62', '0000-00-00', 'Rua JosÃ© Amato', 'SP', 'sp ', 'Cidade Tiradentes', 54, 'A', NULL, 'Teatro', '', NULL, 'yes');
 
 --
 -- Indexes for dumped tables
@@ -113,13 +112,13 @@ ALTER TABLE `tbprestador`
 -- AUTO_INCREMENT for table `tbcliente`
 --
 ALTER TABLE `tbcliente`
-  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `tbprestador`
 --
 ALTER TABLE `tbprestador`
-  MODIFY `idPrestador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idPrestador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
