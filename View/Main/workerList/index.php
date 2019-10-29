050e0273d05b3c0478a1b199a9e334e954480366
Bruno Silva <brucandido@live.com>
2019-10-29 07:52:37 -0300

    worker profile picture at workerList

--- View/Main/workerList/index.php
+++ View/Main/workerList/index.php
@@ -145,7 +145,7 @@
 
                     ?>
                         <div class="list-item">
-                            <img src="../_img/user_profile_picture/user.svg" alt="user profile" width="130">
+                            <img src="<?php echo $row_worker['profile_picture']; ?>" alt="user profile" width="130">
                             <h6><strong><?php echo $row_worker['full_name']; ?></strong></h6>
                             <a href="../workerProfile/?occupation_subcategory=<?php echo $_GET['occupation_subcategory']; ?>&id_service=<?php echo $_GET['id_service']?>&id_user=<?php echo $row_worker['id']; ?>" class="btn waves-effect waves-light">Ver perfil</a>
                         </div>
